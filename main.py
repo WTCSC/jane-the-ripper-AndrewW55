1fdc92a3aa9e725d2736ee38a239224fef3ad117
AndrewW55 <andrew.d.wheeler09@gmail.com>
2025-11-25 13:26:48 -0700

    code

---
+++ main.py
@@ -0,0 +1,38 @@
+import hashlib
+import csv
+
+hashes = set()
+wordlist = []
+wordhashes = []
+
+
+def hashOpener(path):
+    try:
+        with open(path) as file:
+            for line in file:
+                hashes.add(line.strip())
+    except FileNotFoundError:
+        print("Error with opening hash file, please try again")
+
+def wordCheck(path):
+    try:
+        with open(path) as file:
+            for line in file:
+                og_line = line.strip()
+                hash = hashlib.md5()
+                hash.update(og_line.encode('utf-8'))
+                hash = hash.hexdigest()
+                if hash in hashes:
+                    print(line)
+    except FileNotFoundError:
+        print("Error finding wordlist file, please try again")
+
+def main():
+    hashpath = input("What is the name to your hash file? ")
+    wordpath = input("What is the name of your wordlist file? ")
+
+    hashOpener(hashpath)
+    wordCheck(wordpath)
+
+
+main()
